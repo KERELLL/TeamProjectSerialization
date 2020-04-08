8f80c405f992e968124e26e724cc4b44ef824e83
KERELLL <36132918+KERELLL@users.noreply.github.com>
2020-04-08 12:07:25 +0300

    Add files via upload

---
+++ TeamTask/TeamTask/Seq/Seq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seq
+{
+    [Serializable]
+    public class Seq1221
+    {
+        public double[] _seq1221;
+
+        public Seq1221(int N)
+        {
+            if (N <= 0)
+            {
+                throw new SeqException("Количество элементов должно быть положительным числом");
+            }
+
+            _seq1221 = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                _seq1221[i] = 1 / Math.Pow(2, i);
+            }
+        }
+    }
+}
---
+++ TeamTask/TeamTask/Seq/SeqException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Seq
+{
+    [Serializable]
+    internal class SeqException : Exception
+    {
+        public SeqException()
+        {
+        }
+
+        public SeqException(string message) : base(message)
+        {
+        }
+
+        public SeqException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SeqException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
